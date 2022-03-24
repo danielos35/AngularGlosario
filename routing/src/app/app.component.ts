14962d5b9bb95092eb4392d85a13d3d16cb7d6bd
Daniel <studiosdopplermusic@gmail.com>
2022-03-23 21:48:02 -0500

    routing

--- routing/src/app/app.component.ts
+++ routing/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routing';
+  
+  // Para rutas debemos de inicializar en el constructor, el router
+  constructor(private rutas:Router){}
+
+  irHome(){
+    
+    //ir al Home  
+    this.rutas.navigate(['']);
+  }
+
+  irPO(){
+    this.rutas.navigate(['pageOne']);
+  }
+
+  irPT(){
+    this.rutas.navigate(['pageTwo']);
+  }
+
+  irPTR(){
+    this.rutas.navigate(['pageThree']);
+  }
 }
